795f2231acff5335f24d88124340d1642d7e7476
misusonu18 <misusonu18@gmail.com>
2020-03-31 16:21:13 +0530

    Header and Footer of the project

--- layout/footer.php
+++ layout/footer.php
@@ -1,8 +1,8 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
-<script src="js/alertify.min.js"></script>
-<script src="js/app.js"></script>
+<script src="/js/alertify.min.js"></script>
+<script src="/js/app.js"></script>
 
 <?php
     if (isset($discountError)) {
--- layout/header.php
+++ layout/header.php
@@ -6,7 +6,7 @@
     <title>Shopping cart</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="css/alertify.min.css">
+    <link rel="stylesheet" href="/css/alertify.min.css">
 </head>
 <body>
     
